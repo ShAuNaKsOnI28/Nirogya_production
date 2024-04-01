66f04de7c7f38cbc7c99bfe21c08b5d521dc02b9
ShAuNaKsOnI28 <shaunaksoni28123@gmail.com>
2024-04-01 17:21:34 +0530

    updated 3

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 // static files
 app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 //port
